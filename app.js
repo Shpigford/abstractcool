9deae2318b7dd3ad05dbb5520987bbcad5d382ee
Josh Pigford <josh@joshpigford.com>
2021-11-18 13:53:22 -0600

    Landing page

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
 app.get('/:seed.png', (req, res) => {
   res.set('Content-Type', 'image/png');
 
@@ -10,7 +14,7 @@ app.get('/:seed.png', (req, res) => {
     width: parseInt(req.query.width) || 500,
     height: parseInt(req.query.height) || 500,
     cellSize: parseInt(req.query.cellSize) || 100,
-    variance: parseInt(req.query.variance) || 1,
+    variance: Number(parseFloat(req.query.variance)).toFixed(4) || 1,
     seed: req.params.seed
   })
     .toCanvas()
